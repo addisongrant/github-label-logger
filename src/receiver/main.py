2c52d23a76ab452bef1a0a3c84a105d0bbf9e7f4
Addison Grant <addison08@gmail.com>
2021-02-20 19:35:41 -0800

    Print request headers in logs

--- src/receiver/main.py
+++ src/receiver/main.py
@@ -36,5 +36,6 @@ def verify_signature(request):
     secret = bytearray(os.environ.get("SECRET_TOKEN"), 'utf-8')
     payload_body = request.data
     signature = 'sha256=' + hmac.new(secret, payload_body, 'sha256').hexdigest()
+    print(request.headers)
     return hmac.compare_digest(signature, request.headers.get('HTTP_X_HUB_SIGNATURE'))
 
